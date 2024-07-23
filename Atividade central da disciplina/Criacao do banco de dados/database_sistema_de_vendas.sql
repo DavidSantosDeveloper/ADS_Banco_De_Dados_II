005be6e22952ccb7f73de79399afa8c9a3fbb9da
Francisco David Santos Sousa <franciscodavidsantossousa@gmail.com>
2024-07-23 19:07:08 -0300

    FEAT: ADD database Sistema de vendas

---
+++ Atividade central da disciplina/Criacao do banco de dados/database_sistema_de_vendas.sql
@@ -0,0 +1,14 @@
+-- Database: Sistema_de_Vendas
+
+-- DROP DATABASE IF EXISTS "Sistema_de_Vendas";
+
+CREATE DATABASE "Sistema_de_Vendas"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
