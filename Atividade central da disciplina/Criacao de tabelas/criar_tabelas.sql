429d5e35cd35d15bcec3bd5baf1f845ba20c6267
Francisco David Santos Sousa <franciscodavidsantossousa@gmail.com>
2024-09-15 11:09:36 -0300

    FIX: atualiza campos de endereco da tabela loja para NOT NULL

--- Atividade central da disciplina/Criacao de tabelas/criar_tabelas.sql
+++ Atividade central da disciplina/Criacao de tabelas/criar_tabelas.sql
@@ -2,13 +2,13 @@ CREATE TABLE loja(
     cod_loja serial not null,
     nome text not null,
     telefone varchar(255) not null,
-    cep varchar(255),
-    pais varchar(255),
-    estado varchar(255),
-    cidade varchar(255),
-    bairro varchar(255),
-    logradouro varchar(255),
-    numero int,
+    cep varchar(255) not null,
+    pais varchar(255) not null,
+    estado varchar(255) not null,
+    cidade varchar(255) not null,
+    bairro varchar(255) not null,
+    logradouro varchar(255)not null,
+    numero int not null,
     constraint pk_loja primary key (cod_loja)
 );
 
--- Atividade central da disciplina/triggers/triggers.sql
+++ Atividade central da disciplina/triggers/triggers.sql
@@ -74,6 +74,82 @@ $valida_cliente$ language plpgsql;
  create or replace trigger trigger_cliente_cadrastro after insert
  on fornecedor for each row execute procedure valida_cliente();
 
- 
 
+
+
+/*
+ ========================================
+ ||                                    ||
+ ||Trigger da tabela    CARGO          ||
+ ||                                    ||
+ ========================================
+ */
+
+ -- >>>>>>>>>>>>>>>>  INSERT
+create or replace function valida_cargo() returns trigger as $valida_cargo$
+begin
+
+    if exists(SELECT cod_cargo from cargo where cod_cargo=new.cod_cargo) is null then
+        raise exception 'Já existe um cargo usando a chave primaria % na tabela! ',new.cod_cargo;
+    end if;
+
+    if new.cod_cargo is null then
+        raise exception 'A chave primaria não pode ser nula!';
+    end if;
+
+    if new.nome is null  then
+        raise exception 'o campo % tem valor inválido!',new.nome;
+    end if;
+
+    return new;
+end;
+$valida_cargo$ language plpgsql;
+
+
+ create or replace trigger trigger_cargo_cadrastro after insert
+ on fornecedor for each row execute procedure valida_cargo();
+
+
+
+/*
+ ========================================
+ ||                                    ||
+ ||Trigger da tabela   LOJA            ||
+ ||                                    ||
+ ========================================
+ */
+
+ -- >>>>>>>>>>>>>>>>  INSERT
+create or replace function valida_loja() returns trigger as $valida_loja$
+begin
+
+    if exists(SELECT cod_loja from loja where cod_loja=new.cod_loja) is null then
+        raise exception 'Já existe uma loja usando a chave primaria % na tabela! ',new.cod_loja;
+    end if;
+
+    if new.cod_loja is null then
+        raise exception 'A chave primaria não pode ser nula!';
+    end if;
+
+    if new.nome is null  then
+        raise exception 'o campo % tem valor inválido!',new.nome;
+    end if;
+     if new.nome is null  then
+        raise exception 'o campo % tem valor inválido!',new.nome;
+    end if;
+     if new.telefone is null  then
+        raise exception 'o campo % tem valor inválido!',new.telefone;
+    end if;
+     if new.nome is null  then
+        raise exception 'o campo % tem valor inválido!',new.nome;
+    end if;
+    
+
+    return new;
+end;
+$valida_cargo$ language plpgsql;
+
+
+ create or replace trigger trigger_cargo_cadrastro after insert
+ on fornecedor for each row execute procedure valida_cargo();
 
