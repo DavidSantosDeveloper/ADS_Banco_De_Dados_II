1354bee6c964507d460524b6225bab2bbdb7f607
Francisco David Santos Sousa <franciscodavidsantossousa@gmail.com>
2024-09-15 10:47:40 -0300

    FEAT: tigger insert cliente base

--- Atividade central da disciplina/triggers/triggers.sql
+++ Atividade central da disciplina/triggers/triggers.sql
@@ -31,7 +31,7 @@ end;
 $valida_fornecedor$ language plpgsql;
 
 
- create or replace trigger trigger_fornecedor_cadrastro after insert OR update
+ create or replace trigger trigger_fornecedor_cadrastro after insert 
  on fornecedor for each row execute procedure valida_fornecedor();
 
 -- >>>>>>>>>>> UPDATE
@@ -41,7 +41,39 @@ $valida_fornecedor$ language plpgsql;
 /*
  ========================================
  ||                                    ||
- ||Trigger da tabela Fornecedor        ||
+ ||Trigger da tabela     CLIENTE       ||
  ||                                    ||
  ========================================
  */
+
+-- >>>>>>>>>>>>>>>>  INSERT
+create or replace function valida_cliente() returns trigger as $valida_cliente$
+begin
+
+    if exists(SELECT cod_cliente from cliente where cod_cliente=new.cod_cliente) is null then
+        raise exception 'Já existe um cliente usando a chave primaria % na tabela! ',new.cod_fornecedor;
+    end if;
+
+    if new.cod_fornecedor is null then
+        raise exception 'A chave primaria não pode ser nula!';
+    end if;
+
+    if new.nome is null  then
+        raise exception 'o campo % tem valor inválido!',new.nome;
+    end if;
+
+    if new.telefone is null then
+        raise exception 'o campo % tem valor inválido!',new.telefone;
+    end if;
+
+    return new;
+end;
+$valida_cliente$ language plpgsql;
+
+
+ create or replace trigger trigger_cliente_cadrastro after insert
+ on fornecedor for each row execute procedure valida_cliente();
+
+ 
+
+
